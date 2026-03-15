14095932aa5068ba785e9e9e2d6c79334a81f611
HylkeH <hylkehoekstra1999@gmail.com>
2026-03-15 15:09:47 +0100

    Update getting_spectra.py

--- getting_spectra.py
+++ getting_spectra.py
@@ -28,7 +28,49 @@ with tarfile.open(tar_path, "r:gz") as tar:
 spectra = np.array(spectra)
 noise = np.array(noise_list)
 
-X = np.stack([spectra, noise], axis=-1) #Convert it to the spectra + noise (1000, N_wavelengths, 2) shape
+x = np.stack([spectra, noise], axis=-1) #Convert it to the spectra + noise (1000, N_wavelengths, 2) shape
 
-print("Spectra shape:", spectra.shape)
-print("Noise shape:", noise.shape)
+from astropy.table import Table
+
+tab = Table.read("/datatab_Woo2024_training.fits.gz") #Read content of table for labels
+#Give labels to values (this is just an example for the one bin (have to generalize if we want to do more)
+labels = []
+
+for i in range(1000):
+
+    fname = f"spec-{i}.fits"
+    row = tab[tab["fname"] == fname][0]
+
+    labels.append([
+        row["logage_in"],
+        row["metal_in"],
+        row["ebv_in"],
+        row["ML_r"]
+    ])
+
+y = np.array(labels)
+
+
+
+#Split into test and validation data 
+from sklearn.model_selection import train_test_split
+
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2)
+
+#Compile the model (make sure to do this in a seperate cell in colab or wherever we run this
+model_builder = StarNet2017()
+model = model_builder.model(x.shape[1]) #Uses the length of the wavelength 
+
+model.compile(
+    optimizer=model_builder.optimizer,
+    loss="mse"
+)
+history = model.fit(
+    x_train,
+    y_train,
+    validation_data=(x_val, y_val),
+    epochs=10, #Change epochs to whatever we want 
+    batch_size=32
+)
+
+
